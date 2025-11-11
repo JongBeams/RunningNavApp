6fa87134d8260d5a20cbab5f47ab0f7dd9236df3
JongBeom Jang <wwhow2003@gmail.com>
2025-11-11 17:59:27 +0900

    Running Nav App Project Create

---
+++ runningnavapp/src/main/java/io/jongbeom/runningnavapp/RunningnavappApplication.java
@@ -0,0 +1,13 @@
+package io.jongbeom.runningnavapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningnavappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningnavappApplication.class, args);
+	}
+
+}
---
+++ runningnavapp/src/test/java/io/jongbeom/runningnavapp/RunningnavappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jongbeom.runningnavapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningnavappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
